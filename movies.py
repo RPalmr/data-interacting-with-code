c523538b2a740547b8591430ba26d1feaf1f9696
reece palmer <reecelpalmer@gmail.com>
2023-10-19 11:21:22 +0100

    Test

--- movies.py
+++ movies.py
@@ -3,7 +3,6 @@ import sqlite3
 conn = sqlite3.connect('data/movies.sqlite')
 db = conn.cursor()
 query = "SELECT * FROM directors"
-# Execute the SQL query
 db.execute(query)
 rows = db.fetchall()
 print(rows)
